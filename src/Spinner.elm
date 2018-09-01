d51153bc797758560613b0ca6d2f43ed7a212784
Stephen Karger <stephen.karger@gmail.com>
2018-09-01 09:24:51 -0400

    Run elm-upgrade. See output below.
    $ elm-upgrade
    INFO: Found elm at /usr/local/bin/elm
    INFO: Found elm 0.19.0
    INFO: Found elm-format at /usr/local/bin/elm-format
    INFO: Found elm-format 0.8.0
    INFO: Cleaning ./elm-stuff before upgrading
    INFO: Converting elm-package.json -> elm.json
    INFO: Detected a package project (this project has exposed modules)
    INFO: Switching from elm-lang/animation-frame (deprecated) to elm/browser
    INFO: Installing latest version of elm/browser
    Here is my plan:
    
      Add:
        elm/browser    1.0.0 <= v < 2.0.0
    
    Would you like me to update your elm.json accordingly? [Y/n]: Y
    Dependencies loaded from local cache.
    Dependencies ready!
    INFO: Switching from elm-lang/core (deprecated) to elm/core
    INFO: Installing latest version of elm/core
    It is already installed!
    INFO: Detected use of elm-lang/core#Time; installing elm/time
    Here is my plan:
    
      Add:
        elm/time    1.0.0 <= v < 2.0.0
    
    Would you like me to update your elm.json accordingly? [Y/n]: Y
    Dependencies loaded from local cache.
    Dependencies ready!
    INFO: Switching from elm-lang/html (deprecated) to elm/html
    INFO: Installing latest version of elm/html
    Here is my plan:
    
      Add:
        elm/html    1.0.0 <= v < 2.0.0
    
    Would you like me to update your elm.json accordingly? [Y/n]: Y
    Dependencies loaded from local cache.
    Dependencies ready!
    WARNING: eskimoblood/elm-color-extra has not been upgraded to 0.19 yet!
    INFO: Upgrading *.elm files in src/
    
    SUCCESS! Your project's dependencies and code have been upgraded.
    However, your project may not yet compile due to API changes in your
    dependencies.
    
    See <https://github.com/elm/compiler/blob/master/upgrade-docs/0.19.md>
    and the documentation for your dependencies for more information.
    
    WARNING! 1 of your dependencies have not yet been upgraded to
    support Elm 0.19.
      - https://github.com/eskimoblood/elm-color-extra

--- src/Spinner.elm
+++ src/Spinner.elm
@@ -1,22 +1,30 @@
-module Spinner exposing (Direction(..), Config, defaultConfig, view, Model, Msg, update, subscription, init)
+module Spinner exposing
+    ( Model, Msg, subscription, init, update, view
+    , Direction(..), Config, defaultConfig
+    )
 
 {-| A highly configurable, efficiently rendered spinner component.
 
 Check the [README for a general introduction into this module](http://package.elm-lang.org/packages/damienklinnert/elm-spinner/latest/).
 
+
 # The Elm Architecture
+
 @docs Model, Msg, subscription, init, update, view
 
+
 # Custom Spinners
+
 @docs Direction, Config, defaultConfig
+
 -}
 
+import AnimationFrame exposing (times)
+import Color exposing (Color, white)
+import Color.Convert exposing (colorToCssRgba)
 import Html exposing (Html, div)
 import Html.Attributes exposing (style)
 import Time exposing (Time)
-import Color exposing (Color, white)
-import Color.Convert exposing (colorToCssRgba)
-import AnimationFrame exposing (times)
 
 
 {-| Contains the current state for the spinner.
@@ -52,10 +60,10 @@ update : Msg -> Model -> Model
 update msg (Model time) =
     case msg of
         Noop ->
-            (Model time)
+            Model time
 
         AnimationFrame newTime ->
-            (Model newTime)
+            Model newTime
 
 
 {-| The actual spinner component.
@@ -63,10 +71,11 @@ update msg (Model time) =
 view : Config -> Model -> Html msg
 view cfg (Model time) =
     let
-        range = List.range 0 ((floor cfg.lines) - 1) |> List.map toFloat
+        range =
+            List.range 0 (floor cfg.lines - 1) |> List.map toFloat
     in
-        div []
-            (List.map (\i -> div [ outerStyle cfg ] [ div [ barStyles cfg time i ] [] ]) range)
+    div []
+        (List.map (\i -> div [ outerStyle cfg ] [ div [ barStyles cfg time i ] [] ]) range)
 
 
 {-| A spinner can spin `Clockwise` or `Counterclockwise`.
@@ -78,22 +87,22 @@ type Direction
 
 {-| A type describing how your spinner looks like.
 
- - `lines`: Number of lines (a value from 5 to 17, default is 13)
- - `length`: line length (a value from 0 to 56, default is 28)
- - `width`: line width (a value from 2 to 52, default is 14)
- - `radius`: distance from origin to beginning of lines (a value from 0 to 84, default is 42)
- - `scale`: scale for the whole spinner (a value from 0 to 5, default is 1)
- - `corners`: roundness of corners (a value from 0 to 1, default is 1)
- - `opacity`: minimum opacity of inactive lines (a value from 0 to 1, default is 0.25)
- - `rotate`: rotate the spinner by some degrees (a value from 0 to 90, default is 0)
- - `direction`: spinner direction (default is Clockwise)
- - `speed`: (a value from 0.5 (slowest), 2.2 (fastest), default is 1)
- - `trail`: how long is the trail after the active line (a value from 10 to 100, default is 60)
- - `translateX`: moves the spinner horizontally (a value from 0 to 100, default is 50)
- - `translateY`: moves the spinner vertically (a value from 0 to 100, default is 50)
- - `shadow`: adds a box shadow (default is True)
- - `hwaccel`: enables hardware acceleration for lines (default is False)
- - `color`: determines the color for each line based on an index parameter (default is `always Color.white`)
+  - `lines`: Number of lines (a value from 5 to 17, default is 13)
+  - `length`: line length (a value from 0 to 56, default is 28)
+  - `width`: line width (a value from 2 to 52, default is 14)
+  - `radius`: distance from origin to beginning of lines (a value from 0 to 84, default is 42)
+  - `scale`: scale for the whole spinner (a value from 0 to 5, default is 1)
+  - `corners`: roundness of corners (a value from 0 to 1, default is 1)
+  - `opacity`: minimum opacity of inactive lines (a value from 0 to 1, default is 0.25)
+  - `rotate`: rotate the spinner by some degrees (a value from 0 to 90, default is 0)
+  - `direction`: spinner direction (default is Clockwise)
+  - `speed`: (a value from 0.5 (slowest), 2.2 (fastest), default is 1)
+  - `trail`: how long is the trail after the active line (a value from 10 to 100, default is 60)
+  - `translateX`: moves the spinner horizontally (a value from 0 to 100, default is 50)
+  - `translateY`: moves the spinner vertically (a value from 0 to 100, default is 50)
+  - `shadow`: adds a box shadow (default is True)
+  - `hwaccel`: enables hardware acceleration for lines (default is False)
+  - `color`: determines the color for each line based on an index parameter (default is `always Color.white`)
 
 -}
 type alias Config =
@@ -146,20 +155,21 @@ defaultConfig =
 outerStyle : Config -> Html.Attribute msg
 outerStyle cfg =
     style
-        ([ ( "position", "absolute" )
-         , ( "top", "calc(" ++ (toString cfg.translateY) ++ "%)" )
-         , ( "left", (toString cfg.translateX) ++ "%" )
-         , ( "transform"
-           , "scale("
+        [ ( "position", "absolute" )
+        , ( "top", "calc(" ++ toString cfg.translateY ++ "%)" )
+        , ( "left", toString cfg.translateX ++ "%" )
+        , ( "transform"
+          , "scale("
                 ++ toString cfg.scale
                 ++ ")"
-                ++ if cfg.hwaccel then
-                    " translate3d(0px, 0px, 0px)"
-                   else
-                    ""
-           )
-         ]
-        )
+                ++ (if cfg.hwaccel then
+                        " translate3d(0px, 0px, 0px)"
+
+                    else
+                        ""
+                   )
+          )
+        ]
 
 
 barStyles : Config -> Float -> Float -> Html.Attribute a
@@ -167,7 +177,8 @@ barStyles cfg time n =
     let
         directionBasedDeg =
             if cfg.direction == Clockwise then
-                (cfg.lines - n)
+                cfg.lines - n
+
             else
                 n
 
@@ -184,10 +195,10 @@ barStyles cfg time n =
             (n / cfg.lines) * fullBlinkTime |> truncate
 
         lineOpacity =
-            (toFloat (1000 - (((truncate time) + movePerLight) % (truncate fullBlinkTime)))) / 1000
+            toFloat (1000 - (modBy (truncate fullBlinkTime) (truncate time + movePerLight))) / 1000
 
         trailedOpacity =
-            (max 0 ((cfg.lines * lineOpacity) - (cfg.lines - scaledTrail))) / scaledTrail
+            max 0 ((cfg.lines * lineOpacity) - (cfg.lines - scaledTrail)) / scaledTrail
 
         borderRadius =
             cfg.corners * cfg.width / 2
@@ -195,28 +206,29 @@ barStyles cfg time n =
         baseLinedOpacity =
             max cfg.opacity trailedOpacity |> toString
     in
-        style
-            [ ( "background", colorToCssRgba (cfg.color n) )
-            , ( "height", (toString cfg.width) ++ "px" )
-            , ( "width", "" ++ (toString (cfg.length + cfg.width)) ++ "px" )
-            , ( "position", "absolute" )
-            , ( "transform-origin", "left" )
-            , ( "transform", "rotate(" ++ deg ++ "deg) translate(" ++ (toString cfg.radius) ++ "px, 0px)" )
-            , ( "border-radius", (toString borderRadius) ++ "px" )
-            , ( "opacity", baseLinedOpacity )
-            , ( "box-shadow"
-              , (if cfg.shadow then
-                    "0 0 4px #000"
-                 else
-                    "none"
-                )
-              )
-              -- TODO add browser prefixes!
-            , ( "-webkit-box-shadow"
-              , (if cfg.shadow then
-                    "0 0 4px #000"
-                 else
-                    "none"
-                )
-              )
-            ]
+    style
+        [ ( "background", colorToCssRgba (cfg.color n) )
+        , ( "height", toString cfg.width ++ "px" )
+        , ( "width", "" ++ toString (cfg.length + cfg.width) ++ "px" )
+        , ( "position", "absolute" )
+        , ( "transform-origin", "left" )
+        , ( "transform", "rotate(" ++ deg ++ "deg) translate(" ++ toString cfg.radius ++ "px, 0px)" )
+        , ( "border-radius", toString borderRadius ++ "px" )
+        , ( "opacity", baseLinedOpacity )
+        , ( "box-shadow"
+          , if cfg.shadow then
+                "0 0 4px #000"
+
+            else
+                "none"
+          )
+
+        -- TODO add browser prefixes!
+        , ( "-webkit-box-shadow"
+          , if cfg.shadow then
+                "0 0 4px #000"
+
+            else
+                "none"
+          )
+        ]
